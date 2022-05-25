838c38ca10c22ebaff8a716abf66dd82d590edb0
Shalu Agrawal <shalu@vinsol.com>
2022-05-25 11:08:28 +0530

    multiply.rb

---
+++ multiply.rb
@@ -0,0 +1,11 @@
+puts "Multiply of two numbers"
+puts "Enter first value: "
+num1=gets.chomp.to_i
+puts "Enter second value: "
+num2=gets.chomp.to_i
+
+# finding sum 
+product=num1*num2
+
+# printing the result
+puts "The diferece is #{product}"
