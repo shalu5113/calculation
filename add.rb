294e72183f1c023377b0fb075d226646cf798236
Shalu Agrawal <shalu@vinsol.com>
2022-05-25 12:31:41 +0530

    Revert "change addition of two numbers file to three numbers"
    This reverts commit cc4d3a85c44b94caf5450a29c3058b6183f29f9f.

--- add.rb
+++ add.rb
@@ -4,16 +4,14 @@ Ruby program to add two numbers.
 
 # input the numbers and converting 
 # them into integer 
-puts "Addition of three numbers"
+puts "Addition of two numbers"
 puts "Enter first value: "
 num1=gets.chomp.to_i
 puts "Enter second value: "
 num2=gets.chomp.to_i
-puts "Enter third value: "
-num3=gets.chomp.to_i
 
 # finding sum 
-sum=num1+num2+num3
+sum=num1+num2
 
 # printing the result
 puts "The sum is #{sum}"
