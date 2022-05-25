e987ae119c41f94395e99a15ef8fe6743effccd9
Shalu Agrawal <shalu@vinsol.com>
2022-05-25 12:47:32 +0530

    adding modulus file

---
+++ general.rb
@@ -0,0 +1,11 @@
+puts "modulus of two numbers"
+puts "Enter first value: "
+num1=gets.chomp.to_i
+puts "Enter second value: "
+num2=gets.chomp.to_i
+
+# finding sum 
+mod=num1%num2
+
+# printing the result
+puts "The sum is #{mod}"
