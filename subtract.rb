fe9844c924b6f1fa201391a0acb3eb56c8056ea2
Shalu Agrawal <shalu@vinsol.com>
2022-05-25 11:05:06 +0530

    difference of two numbers

---
+++ subtract.rb
@@ -0,0 +1,11 @@
+puts "Difference of two numbers"
+puts "Enter first value: "
+num1=gets.chomp.to_i
+puts "Enter second value: "
+num2=gets.chomp.to_i
+
+# finding sum 
+diff=num1-num2
+
+# printing the result
+puts "The diferece is #{diff}"
